a0e8b03f996762463eba5f8b41c56f70bc0e17b2
gaboxxz <gaboxxz@gmail.com>
2020-08-21 18:02:35 -0300

    Book detail screen

--- src/app/screens/Home/screens/BookDetail/index.tsx
+++ src/app/screens/Home/screens/BookDetail/index.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
-function BookDetail({ route, navigation }: any) {
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import styles from './styles';
+
+function BookDetail({ route }: any) {
   // const navigation: any = useNavigation();
   const { book } = route.params;
   return (
-    <View>
-      <Text>{book.title}</Text>
-      <Text>{book.author}</Text>
-      <Image
-        source={{
-          uri: book.imageUrl
-        }}
-        resizeMode="cover"
-      />
-      <View>
-        <Button title="Go home" onPress={() => navigation.navigate('Home')} />
+    <View style={styles.container}>
+      <View style={styles.bookInfoContainer}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: book.imageUrl
+          }}
+          resizeMode="cover"
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.bookTitleText}>{book.title}</Text>
+          <Text style={styles.statusText}>Available</Text>
+          <Text style={styles.bookInfoText}>{book.author}</Text>
+          <Text style={styles.bookInfoText}>{book.year}</Text>
+          <Text style={styles.bookInfoText}>{book.genre}</Text>
+        </View>
+      </View>
+      <View style={styles.buttonsView}>
+        <TouchableOpacity style={[styles.buttonDefault, styles.addToWishListButton]}>
+          <Text style={styles.buttonText}>ADD TO WHISHLIST</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.buttonDefault, styles.rentButton]}>
+          <Text>RENT</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
