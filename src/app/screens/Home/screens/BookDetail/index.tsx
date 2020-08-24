12361f6d05648a91612786b53ba7e7908f2caa28
gaboxxz <gaboxxz@gmail.com>
2020-08-24 13:20:17 -0300

    Fix mixed file from other pr

--- src/app/screens/Home/screens/BookDetail/index.tsx
+++ src/app/screens/Home/screens/BookDetail/index.tsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import styles from './styles';
-
-function BookDetail({ route }: any) {
-  // const navigation: any = useNavigation();
+function BookDetail({ route, navigation }: any) {
   const { book } = route.params;
   return (
-    <View style={styles.container}>
-      <View style={styles.bookInfoContainer}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: book.imageUrl
-          }}
-          resizeMode="cover"
-        />
-        <View style={styles.textContainer}>
-          <Text style={styles.bookTitleText}>{book.title}</Text>
-          <Text style={styles.statusText}>Available</Text>
-          <Text style={styles.bookInfoText}>{book.author}</Text>
-          <Text style={styles.bookInfoText}>{book.year}</Text>
-          <Text style={styles.bookInfoText}>{book.genre}</Text>
-        </View>
-      </View>
-      <View style={styles.buttonsView}>
-        <TouchableOpacity style={[styles.buttonDefault, styles.addToWishListButton]}>
-          <Text style={styles.buttonText}>ADD TO WHISHLIST</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.buttonDefault, styles.rentButton]}>
-          <Text>RENT</Text>
-        </TouchableOpacity>
-      </View>
+    <View>
+      <Text>{book.title}</Text>
+      <Text>{book.author}</Text>
+      <Button title="Go Home" onPress={() => navigation.navigate('Home')} />
     </View>
   );
 }
