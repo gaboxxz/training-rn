4d27c5bd48cd1de8101c1662b348302ab818fc4c
gaboxxz <gaboxxz@gmail.com>
2020-08-26 17:03:31 -0300

    comments table init

--- src/app/screens/Home/screens/BookDetail/index.tsx
+++ src/app/screens/Home/screens/BookDetail/index.tsx
@@ -4,6 +4,7 @@ import { View, Text } from 'react-native';
 import { RootStackParamList } from '@interfaces/navigation';
 import { RouteProp } from '@react-navigation/native';
 import * as routes from '@constants/routes';
+import CommentsTable from './components/CommentsTable';
 
 type BookDetailScreenRouteProp = RouteProp<RootStackParamList, typeof routes.BookDetail>;
 interface Props {
@@ -12,9 +13,10 @@ interface Props {
 function BookDetail({ route }: Props) {
   const { book } = route.params;
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Text>{book.title}</Text>
       <Text>{book.author}</Text>
+      <CommentsTable />
     </View>
   );
 }
