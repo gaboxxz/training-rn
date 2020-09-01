20991d166a004c6264c8440f3adeb3f4cba5f7af
gaboxxz <gaboxxz@gmail.com>
2020-09-01 16:08:26 -0300

    pr fixes

--- src/app/screens/Home/screens/BookDetail/components/CommentsTable/index.tsx
+++ src/app/screens/Home/screens/BookDetail/components/CommentsTable/index.tsx
@@ -1,30 +1,17 @@
 import React, { useState } from 'react';
-import { Text, ListRenderItem, View, Image } from 'react-native';
+import { Text, ListRenderItem } from 'react-native';
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
 import { bookCommentList } from '@services/bookService';
 import { BookComment } from '@interfaces/books';
 import styles from './styles';
-import maleUserImage from './assets/img_user1@3x.png.png';
-import femaleUserImage from './assets/img_user2@3x.png.png';
+import CommentItem from './components/CommentItem';
 
 function CommentsTable() {
   const [state, setState] = useState({ amount: 2, buttonVisible: true, scrollEnabled: false });
 
   const handlePress = () => setState({ amount: 5, buttonVisible: false, scrollEnabled: true });
   const keyExtractor = (_item: BookComment, index: number) => `list-item-${index}`;
-  const renderItem: ListRenderItem<BookComment> = ({ item }) => (
-    <View style={styles.bookCommentContainer}>
-      <Image
-        source={item.gender === 'F' ? femaleUserImage : maleUserImage}
-        resizeMode="cover"
-        style={styles.bookCommentImage}
-      />
-      <View style={styles.bookCommentTextContainer}>
-        <Text style={styles.bookCommnetAuthor}>{item.author}</Text>
-        <Text style={styles.bookCommentText}>{item.text}</Text>
-      </View>
-    </View>
-  );
+  const renderItem: ListRenderItem<BookComment> = ({ item }) => <CommentItem comment={item} />;
   return (
     <>
       <FlatList
@@ -36,7 +23,7 @@ function CommentsTable() {
         keyExtractor={keyExtractor}
       />
       {state.buttonVisible ? (
-        <TouchableOpacity onPress={() => handlePress()}>
+        <TouchableOpacity onPress={handlePress}>
           <Text style={styles.viewAllButton}>View All</Text>
         </TouchableOpacity>
       ) : (
--- src/app/screens/Home/screens/BookDetail/components/CommentsTable/styles.ts
+++ src/app/screens/Home/screens/BookDetail/components/CommentsTable/styles.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native';
-import { grey, secondaryBlue } from '@constants/colors';
+import { secondaryBlue } from '@constants/colors';
 
 export default StyleSheet.create({
   container: {
@@ -9,30 +9,5 @@ export default StyleSheet.create({
     color: secondaryBlue,
     alignSelf: 'center',
     fontSize: 18
-  },
-  bookCommentContainer: {
-    flexDirection: 'row',
-    marginBottom: 20
-  },
-  bookCommentTextContainer: {
-    flex: 1,
-    marginLeft: 10,
-    borderBottomColor: grey,
-    borderBottomWidth: 1
-  },
-  bookCommentImage: {
-    height: 60,
-    width: 60,
-    borderRadius: 40,
-    alignSelf: 'flex-start'
-  },
-  bookCommnetAuthor: {
-    fontWeight: 'bold',
-    marginBottom: 10,
-    fontSize: 18
-  },
-  bookCommentText: {
-    marginBottom: 10,
-    fontSize: 15
   }
 });
